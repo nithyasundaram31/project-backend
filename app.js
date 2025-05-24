c3d4ab2492f7bd4cfb70016a75404311b4e81ff1
nithya <nithukitty112@gmail.com>
2025-05-24 18:06:26 +0530

    chnage

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://keen-sunburst-3cfa0d.netlify.app/', // Replace with your frontend URL
+    origin: 'https://keen-sunburst-3cfa0d.netlify.app', // Replace with your frontend URL
     credentials: true, // Allow credentials to be sent
 }));
 
