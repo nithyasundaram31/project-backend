43bd74010b7558536772fb2624d04e1fb83c05ae
nithya <nithukitty112@gmail.com>
2025-05-24 11:48:38 +0530

    cors i

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app=express();
 
 app.use(cors({
-    origin: 'https://chipper-donut-48ff99.netlify.app', // Replace with your frontend URL
+    origin: 'https://superlative-alfajores-462fe1.netlify.app', // Replace with your frontend URL
     credentials: true, // Allow credentials to be sent
 }));
 
