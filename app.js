345a71023a313d61c859d800f06000d66cc2d0ad
nithya <nithukitty112@gmail.com>
2025-05-27 16:51:38 +0530

    cors cahnge

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://illustrious-starship-a7649b.netlify.app', // Replace with your frontend URL
+    origin: 'https://fancy-mousse-a6900b.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 }));
 
