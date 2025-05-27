a108a743954ebb342619826f1e9ade59e282715c
nithya <nithukitty112@gmail.com>
2025-05-28 00:01:13 +0530

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://fancy-mousse-a6900b.netlify.app', // Replace with your frontend URL
+    origin: 'https://incomparable-pudding-d2369d.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 }));
 
