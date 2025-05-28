aa9456b488c71a8d82abd1480795744f50f923f6
nithya <nithukitty112@gmail.com>
2025-05-28 22:44:52 +0530

    c

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://golden-crumble-ad9e42.netlify.app', // Replace with your frontend URL
+    origin: 'https://delicate-twilight-b001ac.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 }));
 
