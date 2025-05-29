c2f211ebb5708b7613b6f0dec05aefdf1075f7c6
nithya <nithukitty112@gmail.com>
2025-05-29 12:19:34 +0530

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://starlit-pika-8cfa24.netlify.app', // Replace with your frontend URL
+    origin: 'https://friendly-snickerdoodle-bffda5.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 }));
 
