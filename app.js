04717424ef524a47dd9ff3f1b7df95d28bfbd0db
nithya <nithukitty112@gmail.com>
2025-05-31 00:07:05 +0530

    c

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://symphonious-rabanadas-97aec8.netlify.app', // Replace with your frontend URL
+    origin: 'https://lucent-muffin-667a3a.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 }));
 
