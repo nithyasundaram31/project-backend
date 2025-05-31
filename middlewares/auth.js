c9b2b5ef2c08918f629460f0e65e816a30d7575a
nithya <nithukitty112@gmail.com>
2025-05-31 10:26:25 +0530

    changes auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,4 @@
-const jwt = require('jsonwebtoken');
-
-const authenticate = () => {
+const authenticate = (role) => {
   return (req, res, next) => {
     const strToken = req.headers.authorization?.split(' ')[1];
     const token = strToken?.replace(/"/g, '');
@@ -10,11 +8,38 @@ const authenticate = () => {
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) return res.status(401).json({ message: 'Invalid token' }); 
 
-      //attach user info in req object 
       req.user = decoded;  
+      // Role check
+      if (role && req.user.role !== role) {
+        return res.status(403).json({ message: 'Forbidden: Insufficient permissions' });
+      }
       next();
     });
   };
 };
 
 module.exports = authenticate;
+
+
+
+
+// const jwt = require('jsonwebtoken');
+
+// const authenticate = () => {
+//   return (req, res, next) => {
+//     const strToken = req.headers.authorization?.split(' ')[1];
+//     const token = strToken?.replace(/"/g, '');
+
+//     if (!token) return res.status(401).json({ message: 'Unauthorized access' });
+
+//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//       if (err) return res.status(401).json({ message: 'Invalid token' }); 
+
+//       //attach user info in req object 
+//       req.user = decoded;  
+//       next();
+//     });
+//   };
+// };
+
+// module.exports = authenticate;
