833cd3836fa7629c190fd00684f002fbc61beee7
nithya <nithukitty112@gmail.com>
2025-05-31 15:48:17 +0530

    c

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cors({
     // origin: 'https://creative-liger-43d611.netlify.app', // Replace with your frontend URL
     // credentials: true, // Allow credentials to be sent
 
-    origin: 'https://glittery-bunny-71c967.netlify.app', // your Netlify frontend URL
+    origin: 'https://aesthetic-custard-ccedaf.netlify.app', // your Netlify frontend URL
   credentials: true,
 //   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 //   allowedHeaders: ['Content-Type', 'Authorization'], 
