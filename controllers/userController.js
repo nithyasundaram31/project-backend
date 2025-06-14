5b67f275262a8c895ef1cd6b3406059ff53bd4d5
nithya <nithukitty112@gmail.com>
2025-06-14 13:48:59 +0530

    changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,53 +1,72 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
-const createToken = require('../utils/tokenUtils')
+const createToken = require('../utils/tokenUtils');
 
-// Register a new user (Student or Admin)
+// Student Register Controller
 exports.registerUser = async (req, res) => {
     try {
         const { name, email, password, role } = req.body;
-        //Hashing the Password
+
+        //   if role is not student (protect from frontend )
+        if (role !== 'student') {
+            return res.status(403).json({ message: "You are not allowed to register as admin." });
+        }
+  //Hashing the Password
         const user = new User({ name, email, password, role });
         await user.save();
-        res.status(201).json({ user: { name, email, role }, message: "user successfully registed" });
+
+        res.status(201).json({ user: { name, email, role }, message: "User successfully registered" });
+
     } catch (error) {
         if (error.code === 11000) {
-    res.status(400).json({ message: "Email is already registered." });
-  } else {
-    res.status(500).json({ message: "Something went wrong." });
-  }
-}
+            res.status(400).json({ message: "Email is already registered." });
+        } else {
+            res.status(500).json({ message: "Something went wrong." });
+        }
+    }
 };
 
-// Login user (Student or Admin)
+//  Login Controller (both Admin & Student)
 exports.loginUser = async (req, res) => {
-
     try {
-        const { email, password } = req.body;
-        // Check if email and password are provided
+        const { email, password ,isStudent } = req.body;
+
         if (!email || !password) {
             return res.status(400).json({ success: false, message: 'Please provide an email and password' });
         }
 
-        // Check if the user exists with the provided email
         const user = await User.findOne({ email }).select('+password');
         if (!user) {
             return res.status(401).json({ success: false, message: 'Invalid credentials. Please try again.' });
         }
 
-        // Compare the provided password with the stored password
         const match = await bcrypt.compare(password, user.password);
         if (!match) {
             return res.status(401).json({ success: false, message: 'Invalid credentials. Please try again.' });
         }
+     
+         if (isStudent && user.role !== 'student') {
+            return res.status(403).json({ message: "Only students can login here." });
+        } 
         const token = await createToken(user);
-        res.status(200).json({ token, user: { name: user.name, email: user.email, role: user.role, id: user._id, examPermission: user.examPermission }, message: "user successfully login" });
+        res.status(200).json({
+            token,
+            user: {
+                name: user.name,
+                email: user.email,
+                role: user.role,
+                id: user._id,
+                examPermission: user.examPermission
+            },
+            message: "User successfully login"
+        });
+
     } catch (err) {
         res.status(400).json({ error: err.message });
     }
 };
 
-// Get user profile (Student or Admin)
+//  Get Profile
 exports.getProfile = async (req, res) => {
     try {
         const user = await User.findById(req.user.userId).select('-password');
@@ -57,7 +76,7 @@ exports.getProfile = async (req, res) => {
     }
 };
 
-// Update student profile
+// Update Profile
 exports.updateProfile = async (req, res) => {
     const { id } = req.params;
     try {
@@ -78,7 +97,7 @@ exports.updateProfile = async (req, res) => {
     }
 };
 
-// Admin: Get all students
+//  Admin: Get all students
 exports.getAllStudents = async (req, res) => {
     try {
         if (req.user.role !== 'admin') {
