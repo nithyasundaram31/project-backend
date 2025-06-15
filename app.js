186898e9d7392c227adaa4be1a7b5866dcda600b
nithya <nithukitty112@gmail.com>
2025-06-15 22:17:10 +0530

    c

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://effervescent-begonia-d4629d.netlify.app', // Replace with your frontend URL
+    origin: 'https://gleaming-taffy-83eac8.netlify.app', // Replace with your frontend URL
     credentials: true, // Allow credentials to be sent
 
 
