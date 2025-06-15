c5af1a3867aac2770ed593b7e78f91cc4fc5503c
nithya <nithukitty112@gmail.com>
2025-06-15 14:45:47 +0530

    cors

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const resultRouter = require("./routers/resultsRoute");
 const app = express();
 
 app.use(cors({
-    origin: 'https://enchanting-jelly-0f6a0b.netlify.app', // Replace with your frontend URL
+    origin: 'https://golden-tartufo-805370.netlify.app', // Replace with your frontend URL
     credentials: true, // Allow credentials to be sent
 
 
